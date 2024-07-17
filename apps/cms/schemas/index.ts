a930e394f352b2808455742253d083e6ee55ba1e
Ole Herland <ole.gogoro@gmail.com>
2024-07-17 15:49:40 +0200

    started rewriting to go+htmlx

--- apps/cms/schemas/index.ts
+++
@@ -1,9 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import book from './book'
-import bookCategory from './bookCategory'
-import project from './project'
-
-export const schemaTypes = [post, author, category, blockContent, book, bookCategory, project]
---
+++ cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "html/template"
+  "io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+  templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+  return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+  return &Templates{
+    templates: template.Must(template.ParseGlob("views/*.html")),
+  }
+}
+
+type Count struct {
+  Count int
+}
+
+func main() {
+  e := echo.New()
+  e.Use(middleware.Logger())
+  e.Static("/static", "static")
+
+  e.Renderer = newTemplate()
+
+  
+  count := Count { Count: 0}
+
+  e.GET("/", func(c echo.Context) error {
+    return c.Render(200, "index", count)
+  })
+
+  e.POST("/count", func(c echo.Context) error {
+    count.Count++
+    return c.Render(200, "count", count)
+  })
+
+
+  e.Logger.Fatal(e.Start(":42069"))
+}
