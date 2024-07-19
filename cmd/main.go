f25fe726f0e521b02d1da2babc89199420bfd22f
Ole Herland <ole.gogoro@gmail.com>
2024-07-19 19:47:22 +0200

    wip new site

--- cmd/main.go
+++ cmd/main.go
@@ -22,8 +22,78 @@ func newTemplate() *Templates {
   }
 }
 
-type Count struct {
-  Count int
+var id = 0;
+type Contact struct {
+  Id int
+  Name string
+  Email string
+}
+
+func newContact(name string, email string) Contact {
+  id++
+  return Contact{
+    Name: name,
+    Email: email,
+    Id: id,
+  }
+}
+
+type Contacts = []Contact
+
+
+type Data struct {
+  Contacts Contacts
+}
+
+func (d *Data) indexOf(id int) int {
+  for i, contact := range d.Contacts {
+    if contact.Id == id {
+      return i
+    }
+  }
+  return -1
+}
+
+func (d Data) hasEmail(email string) bool {
+  for _, contact := range d.Contacts{
+    if (contact.Email == email) {
+      return true
+    }
+  }
+  return false
+}
+
+func newData() Data {
+  return Data{
+    Contacts: []Contact{
+      newContact("Ole", "ole@herland.me"),
+      newContact("Linn-Tone", "linntone.ellefsen@gmail.com"),
+    },
+  }
+}
+
+type FormData struct {
+  Values map[string]string
+  Errors map[string]string
+}
+
+func newFormData() FormData {
+  return FormData{
+    Values: make(map[string]string),
+    Errors: make(map[string]string),
+  }
+}
+
+type Page struct {
+  Data Data
+  Form FormData
+}
+
+func newPage() Page {
+  return Page{
+    Data: newData(),
+    Form: newFormData(),
+  }
 }
 
 func main() {
@@ -32,19 +102,63 @@ func main() {
   e.Static("/static", "static")
 
   e.Renderer = newTemplate()
-
   
-  count := Count { Count: 0}
+//  page := newPage()
 
   e.GET("/", func(c echo.Context) error {
-    return c.Render(200, "index", count)
+    return c.Render(200, "index", nil)
   })
 
-  e.POST("/count", func(c echo.Context) error {
-    count.Count++
-    return c.Render(200, "count", count)
+  e.GET("/projects", func(c echo.Context) error {
+    return c.Render(200, "projects-page", nil)
   })
 
+  e.GET("/thoughts", func(c echo.Context) error {
+    return c.Render(200, "thoughts-page", nil)
+  })
+
+  e.GET("/about-me", func(c echo.Context) error {
+    return c.Render(200, "about-page", nil)
+  })
+
+/*
+  e.POST("/contacts", func(c echo.Context) error {
+    name := c.FormValue("name")
+    email := c.FormValue("email")
+
+    if (page.Data.hasEmail(email)) {
+      formData := newFormData()
+      formData.Values["name"] = name
+      formData.Values["email"] = email
+      formData.Errors["email"] = "Email already exists"
+
+      return c.Render(422, "form", formData) 
+    }
+    
+    contact := newContact(name, email)
+    page.Data.Contacts = append(page.Data.Contacts, contact)
+    c.Render(200, "form", newFormData())
+    return c.Render(200, "oob-contact", contact)
+  })
+
+  e.DELETE("/contacts/:id", func(c echo.Context) error {
+    idStr := c.Param("id")
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+      return c.String(400, "Invalid id")
+    }
+   
+    index := page.Data.indexOf(id)
+    if (index == -1) {
+      return c.String(404, "Contact not found")
+    }
+
+    page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+    return c.NoContent(200)
+  })
+
+  */
 
   e.Logger.Fatal(e.Start(":42069"))
 }
