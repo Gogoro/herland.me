989fb36330bee20a331a00a86fef3da5aa8b0d24
Ole Andreas Jørnsen Herland <ole.gogoro@gmail.com>
2025-10-15 14:49:18 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://herland.me",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
